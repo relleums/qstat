16b5efd226c0dcf1d51456aa7e5e243c7fd373f7
sebastian <sebmuell@phys.ethz.ch>
2017-09-01 23:56:31 +0200

    make it a pip installable pyhon package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name='qstat',
+    version='0.0.0',
+    description='Parse qstat xml to python dict',
+    url='https://github.com/relleums/qstat',
+    author='Sebastian Achim Mueller',
+    author_email='sebmuell@phys.ethz.ch',
+    license='GPLv3',
+    packages=[
+        'qstat',
+    ],
+    package_data={
+        'qstat': [
+            'tests/resources/*',
+        ]
+    },
+    install_requires=[
+        'docopt',
+        'scipy',
+        'sklearn',
+        'scikit-image',
+        'matplotlib',
+        'pyfact',
+        'pandas',
+        'tqdm',
+        'ujson',
+    ],
+    zip_safe=False,
+)
