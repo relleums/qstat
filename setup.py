7d25df5494676f8a056dda17f28defd964b148e6
Sebastian A. Mueller <sebmuell@phys.ethz.ch>
2017-09-02 02:36:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='qstat',
-    version='0.0.0',
+    version='0.0.1',
     description='Parse qstat xml to python dict',
-    url='https://github.com/relleums/qstat',
+    url='https://github.com/relleums/qstat/',
     author='Sebastian Achim Mueller',
     author_email='sebmuell@phys.ethz.ch',
     license='GPLv3',
