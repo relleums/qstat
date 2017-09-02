bab59f4aa5a6390b9a5e12d8a7ef07668331c852
sebastian <sebmuell@phys.ethz.ch>
2017-09-02 03:28:21 +0200

    try to make the wheel whatever

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from distutils.core import setup
-
+from setuptools import setup
 setup(
     name='qstat',
     version='0.0.1',
     description='Parse qstat xml to python dict',
-    url='https://github.com/relleums/qstat/archive/0.0.2.tar.gz',
+    url='https://github.com/relleums/qstat/archive/0.0.3.tar.gz',
     author='Sebastian Achim Mueller',
     author_email='sebmuell@phys.ethz.ch',
     license='GPLv3',
