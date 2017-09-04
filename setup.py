91cb345b3552d56bd0c432484ef755e759b035a2
sebastian <sebmuell@phys.ethz.ch>
2017-09-04 14:45:09 +0200

    bumb version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='qstat',
-    version='0.0.1',
+    version='0.0.4',
     description='Parse qstat xml to python dict',
     url='https://github.com/relleums/qstat/archive/0.0.3.tar.gz',
     author='Sebastian Achim Mueller',
