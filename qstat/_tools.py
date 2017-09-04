379f52a3ef145bb9267825b162b1e181a1b69688
sebastian <sebmuell@phys.ethz.ch>
2017-09-04 14:43:33 +0200

    numpy like doc strings

--- qstat/_tools.py
+++ qstat/_tools.py
@@ -3,56 +3,71 @@ import xmltodict
 from datetime import datetime
 
 
-def qstat(qstat_path='qstat', typed=True):
-    xml = qstat2xml(qstat_path=qstat_path)
-    if typed:
-        return typed_job_infos(xml2job_infos(xml))
-    else:
-        return xml2job_infos(xml)
-
-
-def qstat2xml(qstat_path='qstat'):
+def qstat(qstat_path='qstat', xml_option='-xml'):
     """
-    Returns a qstat xml string
+    Parameters
+    ----------
+    qstat_path : string
+        The path to the qstat executable.
+
+    Returns
+    -------
+    queue_info : list
+        A list of jobs in 'queue_info'. Jobs are dictionaries with both string 
+        keys and string names.
+    job_info : list
+        A list of jobs in 'job_info'.
+    """
+    xml = qstat2xml(qstat_path=qstat_path)
+    return xml2queue_and_job_info(xml)
+
+
+def qstat2xml(qstat_path='qstat', xml_option='-xml'):
+    """
+    Parameters
+    ----------
+    qstat_path : string
+        The path to the qstat executable.
+
+    Returns
+    -------
+    qstatxml : string
+        The xml stdout string of the 'qstat -xml' call.
     """
     try:
-        out = sp.check_output([qstat_path, '-xml'], stderr=sp.STDOUT)
+        qstatxml = sp.check_output([qstat_path, xml_option], stderr=sp.STDOUT)
     except sp.CalledProcessError as e:
         print('qstat returncode:', e.returncode)
         print('qstat std output:', e.output)
         raise
     except FileNotFoundError as e:
-        e.message = 'Maybe qstat is not installed.'
+        e.message = 'Maybe "'+qstat_path+' '+xml_option+'" is not installed.'
         raise
-    return out
+    return qstatxml
 
 
-def xml2job_infos(qstatxml):
+def xml2queue_and_job_info(qstatxml):
     """
-    Takes qstat xml string and returns a list of dicts of 
-    qsub job name and qsub job state 
+    Parameters
+    ----------
+    qstatxml : string
+        The xml string of the 'qstat -xml' call.
+
+    Returns
+    -------
+    queue_info : list
+        A list of jobs in 'queue_info'. Jobs are dictionaries with both string 
+        keys and string names.
+    job_info : list
+        A list of jobs in 'job_info'.
     """
     x = xmltodict.parse(qstatxml)
-    job_infos = []
+    queue_info = []
     if x['job_info']['queue_info'] is not None:
         for job in x['job_info']['queue_info']['job_list']:
-            job_infos.append(dict(job))
+            queue_info.append(dict(job))
+    job_info = []
     if x['job_info']['job_info'] is not None:
         for job in x['job_info']['job_info']['job_list']:
-            job_infos.append(dict(job))
-    return job_infos
-
-
-def typed_job_infos(job_infos):
-    j = job_infos
-    for i in range(len(j)):
-        if 'JB_job_number' in j[i]:
-            j[i]['JB_job_number'] = int(j[i]['JB_job_number'])
-        if 'JAT_prio' in j[i]:
-            j[i]['JAT_prio'] = float(j[i]['JAT_prio'])
-        if 'JB_submission_time' in j[i]:
-            j[i]['JB_submission_time'] = datetime.strptime(
-                j[i]['JB_submission_time'], 
-                '%Y-%m-%dT%H:%M:%S'
-            )
-    return j
+            job_info.append(dict(job))
+    return queue_info, job_info
