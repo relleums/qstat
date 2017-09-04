7bc0c6e6632edcef5ac2830f6416a51737e833b1
sebastian <sebmuell@phys.ethz.ch>
2017-09-04 16:47:33 +0200

    bumb the release number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='qstat',
     version='0.0.4',
     description='Parse qstat xml to python dict',
-    url='https://github.com/relleums/qstat/archive/0.0.3.tar.gz',
+    url='https://github.com/relleums/qstat/archive/0.0.4.tar.gz',
     author='Sebastian Achim Mueller',
     author_email='sebmuell@phys.ethz.ch',
     license='GPLv3',
