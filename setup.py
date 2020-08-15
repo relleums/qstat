d6169ee725db1aee63e08b39f4664904da8aa3fc
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-08-15 20:49:01 +0200

    now at MPIK

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Parse qstat xml to python dict',
     url='https://github.com/relleums/qstat/archive/0.0.4.tar.gz',
     author='Sebastian Achim Mueller',
-    author_email='sebmuell@phys.ethz.ch',
+    author_email='sebastian-achim.mueller@mpi-hd.mpg.de',
     license='GPLv3',
     packages=[
         'qstat',
